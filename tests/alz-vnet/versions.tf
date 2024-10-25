956db2eb7e11ff84ffb899918669c6a355615f04
noor-shimul <Noor.Shimul@justice.gov.uk>
2024-10-25 16:05:29 +0100

    upgrade tf version

--- tests/alz-vnet/versions.tf
+++ tests/alz-vnet/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  required_version = "=1.5.7"
+  required_version = "=1.9.8"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
